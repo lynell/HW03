9084cd3a9740bf8faa6095045aead1844e9d64ab
Lynell <lynellamanna@live.com>
2015-08-07 01:40:59 +0530

    HW03

--- HW03_ex06.py
+++ HW03_ex06.py
@@ -6,7 +6,13 @@
 ################################################################################
 # Exercise 1
 # When you submit only include your final function: compare
-
+def compare(x,y):
+    if x > y:
+        return 1
+    elif x == y:
+        return 0
+    else:
+        return -1
 
 
 
@@ -15,7 +21,10 @@
 # Exercise 2
 # When you submit only include your final function: hypotenuse
 # Do develop incrementally. Do not share here.
-
+def hypotenuse(x,y):
+    import math 
+    c = math.sqrt(x*x + y*y) #formula to find the hypotenuse of the triangle
+    return c
 
 
 
@@ -23,7 +32,11 @@
 ################################################################################
 # Exercise 3
 # When you submit only include your final function: is_between
-
+def is_between(x, y, z):
+    if x <= y and y <= z:
+        return True
+    else:
+        return False
 
 
 
@@ -31,15 +44,36 @@
 ################################################################################
 # Exercise 6
 # When you submit only include your final function: is_palindrome
-
-
+def is_palindrome(a):
+    if a == a[::-1]:
+        return True
+    else:
+        return False
+        
 
 
 
 ################################################################################
 # Exercise 7
 # When you submit only include your final function: is_power
+def is_power(a, b):
+    if div(a,b) == True:
+        return True
+    else:
+        return False
+        
 
+def div(a,b):
+    if a%b == 0:
+        if a >= b:
+            a = a/b
+            div(a,b)
+        return True
+    else:
+        return False
+
+
+    
 
 
 
@@ -60,28 +94,28 @@ def main():
     ############################################################################
     # Uncomment the below to test and before commiting:
     # # Exercise 1
-    # compare(1,1)
-    # compare(1,2)
-    # compare(2,1)
+    print compare(1,1)
+    print compare(1,2)
+    print compare(2,1)
     # # Exercise 2
-    # hypotenuse(1,1)
-    # hypotenuse(3,4)
-    # hypotenuse(1.2,12)
+    print hypotenuse(1,1)
+    print hypotenuse(3,4)
+    print hypotenuse(1.2,12)
     # # Exercise 3
-    # is_between(1,2,3)
-    # is_between(2,1,3)
-    # is_between(3,1,2)
-    # is_between(1,1,2)
+    print is_between(1,2,3)
+    print is_between(2,1,3)
+    print is_between(3,1,2)
+    # print is_between(1,1,2)
     # # Exercise 6
-    # is_palindrome("Python")
-    # is_palindrome("evitative")
-    # is_palindrome("sememes")
-    # is_palindrome("oooooooooooo")
+    print is_palindrome("Python")
+    print is_palindrome("evitative")
+    print is_palindrome("sememes")
+    print is_palindrome("oooooooooooo")
     # # Exercise 7
-    # is_power(28,3)
-    # is_power(27,3)
-    # is_power(248832,12)
-    # is_power(248844,12)
+    print is_power(28,3)
+    print is_power(27,3)
+    print is_power(248832,12)
+    print is_power(248844,12)
 
 
 if __name__ == "__main__":
--- HW03_string1.py
+++ HW03_string1.py
@@ -25,8 +25,10 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
-  return
+  if count >= 10:
+    return "Number of donuts: many"
+  else:
+    return "Number of donuts: " + str(count)
 
 
 # B. both_ends
@@ -34,9 +36,11 @@ def donuts(count):
 # and the last 2 chars of the original string,
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
-def both_ends(s):
-  # +++your code here+++
-  return
+def both_ends(a):
+  if len(a)>= 2:
+    return a[0:2]+ a[-2:]
+  else:
+    return ""
 
 
 # C. fix_start
@@ -49,8 +53,9 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
+  a = s[0]
+  b = s.replace(a,"*" )
+  return a+ b[1:]
 
 
 # D. MixUp
@@ -61,8 +66,10 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  c = a[0:2]
+  d = b[0:2]
+  return d + a[2:] + " " + c + b[2:]
+  
 
 
 # Provided simple test() function used in main() to print
